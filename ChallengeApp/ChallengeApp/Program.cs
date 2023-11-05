2d85a44796b5dc4f0beec02c67036040c1346db0
User <User@DESKTOP-ITK88ON>
2023-11-05 18:39:42 +0100

    Homework Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 33;
+//Man, man, women, Women
+string gender = "women";
+
+
+if (gender == "Women" || gender == "women")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        if (name == "Ewa" && age == 33) 
+            {
+            Console.WriteLine("Jestem w lini 9");
+            }
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Jestem w lini 9");
+    }
+    else
+    {
+        Console.WriteLine("END PROGRAM ");
+    }
+}
