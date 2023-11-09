9c6262a27f52e2fb4886898f165d9b39a712feeb
User <User@DESKTOP-ITK88ON>
2023-11-09 10:43:03 +0100

    Metoda+Klasa Zewnętrzna (Dzien6)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,6 @@
-﻿User user1 = new User("Adam", "2342423");
+﻿using ChallengeApp;
+
+User user1 = new User("Adam", "2342423");
 User user2 = new User("Dzordz","PAS123");
 User user3 = new User("Dzordz", "1234qwer");
 User user4 = new User("Anna", "112111");
@@ -10,35 +12,4 @@ user1.AddScore(2);
 var result = user1.Result;
 Console.WriteLine(result);
 
-//static zmienne 
-var name = User.GameName;
-var pi = Math.PI;
 
-
-class User
-{
-    public static string GameName = "Diablo";
-
-    private List<int> score = new List<int>();
-    
-    public User(string login, string password)
-    {
-        this.Login = login;
-        this.Password = password;
-    }
-    public string Login { get; private set; } 
-    public string Password { get; private set; }
-
-    public int Result 
-    { 
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-    }
-
-}
