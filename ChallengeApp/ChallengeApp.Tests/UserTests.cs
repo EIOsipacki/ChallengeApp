4a29605cd0b8b2d13759c61faf3ff78dda1f33bd
User <User@DESKTOP-ITK88ON>
2023-11-13 07:32:22 +0100

    Testowanie. UnitTest. Dzień7

---
+++ ChallengeApp/ChallengeApp.Tests/UserTests.cs
@@ -0,0 +1,58 @@
+using System.Reflection.Metadata;
+
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenUserCollectTwoSetOfPoints_ShouldCorrectSumAsResult()
+        {
+            // arrange
+            var user = new User("Adam", "adad123");
+            user.AddScore(5);
+            user.AddScore(6);
+
+            // act
+            var result = user.Result;
+
+            //assert
+
+            Assert.AreEqual(11, result);
+        }
+        [Test]
+        public void WhenUserCollectTwoSetOfPoints_ShouldCorrectSumAsResult1()
+        {
+            // arrange
+            var user = new User("Adam", "adad123");
+            user.AddScore(5);
+            user.AddScore(2);
+
+            // act
+            var result = user.Result;
+
+            //assert
+
+            Assert.AreEqual(7, result);
+        }
+        [Test]
+        public void WhenUserCollectWithUemnyPunktSetOfPoints_ShouldNotCorrectSumAsResult__()
+        {
+            // arrange
+            var user = new User("Adam", "adad123");
+            user.AddScore(5);
+            user.AddScore(-6);
+
+            // act
+            var result = user.Result;
+
+            //assert
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
