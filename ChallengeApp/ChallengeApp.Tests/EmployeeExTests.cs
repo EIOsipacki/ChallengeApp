5cfe0d4543bd3dc97b3b381b0e09f612a0dce16a
User <User@DESKTOP-ITK88ON>
2023-11-18 20:19:05 +0100

    HW Dzień 9 - 3 testy

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeExTests.cs
@@ -0,0 +1,51 @@
+﻿namespace ChallengeApp.Tests; 
+
+public class EmployeeExTests
+{
+    [Test]
+    public void GdyDodajemyOcenyPracownikowiResultaMusiDawacMaxZOcen()
+    {
+        //arrange
+        var employee = new EmployeeEx("Adam", "Kamizelich");
+        employee.AddGrades(0);
+        employee.AddGrades(0);
+        employee.AddGrades(0);
+        // act
+
+        var statistics = employee.GetStatistics(); 
+
+        //assert
+        Assert.AreEqual(0,statistics.Max);
+    }
+    [Test]
+    public void GdyDodajemyOcenyPracownikowiResultaMusiDawacMinimalnaZOcen()
+    {
+        //arrange
+        var employee = new EmployeeEx("Adam", "Kamizelich");
+        employee.AddGrades(2);
+        employee.AddGrades(4);
+        employee.AddGrades(6);
+        // act
+
+        var statistics = employee.GetStatistics();
+
+        //assert
+        Assert.AreEqual(2, statistics.Min);
+    }
+    [Test]
+    public void GdyDodajemyOcenyPracownikowiResultaMusiDawacSredniaOcen()
+    {
+        //arrange
+        var employee = new EmployeeEx("Adam", "Kamizelich");
+        employee.AddGrades(2);
+        employee.AddGrades(1);
+        employee.AddGrades(6);
+        // act
+
+        var statistics = employee.GetStatistics();
+
+        //assert
+        Assert.AreEqual((2+1+6)/3, statistics.Average);
+    }
+
+}
